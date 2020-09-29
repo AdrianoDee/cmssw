2f94e549f8e477f871439cad1abf736c76c502cb
AdrianoDee <adriano.diflorio@ba.infn.it>
2020-09-29 15:56:07 +0200

    Pixel Geometry Updates for Phase2 - adding phase2PixelGeometry namespace - increasing numbers for max hits & max tracks for vtx - increasing the number of histograms for phase2 pixel layers - increasing max number of hits of pixel track to 24 (maybe will need to lower it)

--- CUDADataFormats/TrackingRecHit/interface/TrackingRecHit2DSOAView.h
+++ CUDADataFormats/TrackingRecHit/interface/TrackingRecHit2DSOAView.h
@@ -15,12 +15,12 @@ namespace pixelCPEforGPU {
 class TrackingRecHit2DSOAView {
 public:
   static constexpr uint32_t maxHits() { return gpuClustering::MaxNumClusters; }
-  using hindex_type = uint16_t;  // if above is <=2^16
+  using hindex_type = uint32_t;  // if above is <=2^16
 
   using Hist =
-      cms::cuda::HistoContainer<int16_t, 128, gpuClustering::MaxNumClusters, 8 * sizeof(int16_t), uint16_t, 10>;
+      cms::cuda::HistoContainer<int16_t, 256, gpuClustering::MaxNumClusters, 8 * sizeof(int16_t), uint32_t, 28>;
 
-  using AverageGeometry = phase1PixelTopology::AverageGeometry;
+  using AverageGeometry = pixelTopology::AverageGeometry;
 
   template <typename>
   friend class TrackingRecHit2DHeterogeneous;
