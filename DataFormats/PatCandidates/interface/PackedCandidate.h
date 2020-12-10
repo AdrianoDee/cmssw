e126da9f4ba14c8a9124ba11ec2fefb14b201128
AdrianoDee <adriano.diflorio@ba.infn.it>
2020-12-10 11:20:25 +0100

    Reverting to exception

--- DataFormats/PatCandidates/interface/PackedCandidate.h
+++ DataFormats/PatCandidates/interface/PackedCandidate.h
@@ -1089,11 +1089,10 @@ namespace pat {
                              "Loading it from a LUT." << std::endl;
       std::call_once(
           covariance_load_flag, [](int v) { covarianceParameterization_.load(v); }, covarianceVersion_);
-      if (covarianceParameterization_.loadedVersion() != covarianceVersion_ && hasTrackDetails()) {
-
-        edm::LogWarning("Multiple Covariance") <<
-               "Attempting to load multiple covariance version in same process. " <<
-               "This is not supported."<< std::endl;
+      if (covarianceParameterization_.loadedVersion() != covarianceVersion_) {
+        throw edm::Exception(edm::errors::UnimplementedFeature)
+            << "Attempting to load multiple covariance version in same process. "
+               "This is not supported.";
       }
       return covarianceParameterization_;
     }
