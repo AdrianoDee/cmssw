353f4653125d5fa0119cc3b35a8864f5624159a8
AdrianoDee <adriano.diflorio@ba.infn.it>
2024-09-04 23:57:09 +0200

    Touch

--- Utilities/RelMon/python/directories2html.py
+++ Utilities/RelMon/python/directories2html.py
@@ -83,7 +83,7 @@ style_location="/cms-service-reldqm"
 def get_page_header(directory=None, standalone=False, additional_header=""):
   style_location="/cms-service-reldqm"
   if standalone:
-    style_location = "https://raw.githubusercontent.com/cms-PdmV/RelMonService2/77c534ec93401ca5de222ac62a6422f02389dafc/report_website/" #RelMonService2 
+    style_location = "http://cms-service-reldqm.web.cern.ch/" + style_location +"/" #RelMonService2 
   javascripts=''
   style=''
   tablestyle=''
@@ -119,10 +119,10 @@ def get_page_header(directory=None, standalone=False, additional_header=""):
   html='<html>'+\
        '<head>'+\
        '<title>RelMon Summary</title>'+\
-       '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>' +\
-       '<link rel="stylesheet" href="screen.css" type="text/css" media="screen, projection">' +\
-       '<link rel="stylesheet" href="print.css" type="text/css" media="print">' +\
-       '<link rel="stylesheet" href="plugins/fancy-type/screen.css" type="text/css" media="screen, projection">' +\
+       '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>' + \
+       '<link rel="stylesheet" href="%s/screen.css" type="text/css" media="screen, projection">'%style_location+\
+       '<link rel="stylesheet" href="%s/print.css" type="text/css" media="print">'%style_location+\
+       '<link rel="stylesheet" href="%s/fancy-type-screen.css" type="text/css" media="screen, projection">'%style_location+\
        '<style type="text/css">'+\
        '.rotation {display: block;-webkit-transform: rotate(-90deg);-moz-transform: rotate(-90deg); }'+\
        '%s'%style+\
