f7b30d7081c028cfe3c06ab0725d725433dd2522
AdrianoDee <adriano.diflorio@ba.infn.it>
2024-09-04 23:43:50 +0200

    Touch

--- Utilities/RelMon/python/directories2html.py
+++ Utilities/RelMon/python/directories2html.py
@@ -120,8 +120,9 @@ def get_page_header(directory=None, standalone=False, additional_header=""):
        '<head>'+\
        '<title>RelMon Summary</title>'+\
        '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>' +\
-       '<link rel="stylesheet" href="%s/screen.css" type="text/css" media="screen, projection">'%blueprint_location+\
-       '<link rel="stylesheet" href="%s/print.css" type="text/css" media="print">'%blueprint_location+\
+       '<link rel="stylesheet" href="screen.css" type="text/css" media="screen, projection">' +\
+       '<link rel="stylesheet" href="print.css" type="text/css" media="print">' +\
+       '<link rel="stylesheet" href="plugins/fancy-type/screen.css" type="text/css" media="screen, projection">' +\
        '<style type="text/css">'+\
        '.rotation {display: block;-webkit-transform: rotate(-90deg);-moz-transform: rotate(-90deg); }'+\
        '%s'%style+\
