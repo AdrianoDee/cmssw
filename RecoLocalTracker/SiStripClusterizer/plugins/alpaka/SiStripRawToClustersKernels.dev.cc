b43ec635b516c9eaa189054b1073897e330a547b
AdrianoDee <adriano.di.florio@cern.ch>
2024-12-13 19:44:18 +0100

    Start using the sequential container

--- RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClustersKernels.dev.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClustersKernels.dev.cc
@@ -27,107 +27,117 @@
 #include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
 #include "HeterogeneousCore/AlpakaInterface/interface/prefixScan.h"
 #include "HeterogeneousCore/AlpakaInterface/interface/workdivision.h"
-#include "RecoLocalTracker/SiPixelClusterizer/interface/SiPixelClusterThresholds.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/OneToManyAssoc.h"
 
-// local includes
-#include "CalibPixel.h"
-#include "ClusterChargeCut.h"
-#include "PixelClustering.h"
-#include "SiStripRawToClustersKernel.h"
+#include "SiStripRawToClustersKernels.h"
 
 // #define GPU_DEBUG
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
   
     // Interface to outside
-    void SiStripRawToClustersKernel<TrackerTraits>::makeStripClustersAsync(
+    void SiStripRawToClustersKernels::makeStripClustersAsync(
         Queue &queue,
         const std::vector<const FEDRawData*>& rawdata,
         const std::vector<std::unique_ptr<sistrip::FEDBuffer>>& buffers,
-        const StripConditionsDetSoAConstView& detView,
-        const StripConditionsStripSoAConstView& stripView,
-        const StripConditionsAPVSoAConstView& apvView)
+        const ::reco::StripConditionsDetSoAConstView& detView,
+        const ::reco::StripConditionsStripSoAConstView& stripView,
+        const ::reco::StripConditionsAPVSoAConstView& apvView)
 
       {
 
-        size_t totalSize{0};
+        int totalSize = 0;
+        int offsetSize = 0;
         for (const auto& buff : buffers) {
           if (buff != nullptr) {
+            offsetSize++;
             totalSize += buff->bufferSize();
           }
         }
 
-        auto fedRawDataHost = cms::cuda::make_host_unique<uint8_t[]>(totalSize, stream);
-        auto fedRawDataGPU = cms::cuda::make_device_unique<uint8_t[]>(totalSize, stream);
+        SequentialContainerView fedMapView;
+        auto fedMap = cms::alpakatools::make_host_buffer<SequentialContainer>();
+        auto fedMapContainer = cms::alpakatools::make_host_buffer<SequentialContainerStorage[]>(totalSize);
+        auto fedMapOffsets = cms::alpakatools::make_host_buffer<SequentialContainerOffsets[]>(offsetSize + 1);
+        
+        fedMapView.assoc = fedMap.data();
+        fedMapView.offStorage = fedMapOffsets.data();
+        fedMapView.contentStorage = fedMapContainer.data();
+        fedMapView.contentSize = alpaka::getExtentProduct(fedMapContainer);
+        fedMapView.offSize = alpaka::getExtentProduct(fedMapOffsets);
+        
 
-        size_t off = 0;
+      //   auto fedRawDataHost = cms::cuda::make_host_unique<uint8_t[]>(totalSize, stream);
+      //   auto fedRawDataGPU = cms::cuda::make_device_unique<uint8_t[]>(totalSize, stream);
 
-        std::vector<stripgpu::fedId_t> fedIndex_;
-        std::vector<size_t> fedRawDataOffsets_;
+      //   size_t off = 0;
 
-        fedRawDataOffsets_.clear();
-        fedIndex_.clear();
-        fedIndex_.resize(sistrip::NUMBER_OF_FEDS, stripgpu::invalidFed);
+      //   std::vector<stripgpu::fedId_t> fedIndex_;
+      //   std::vector<size_t> fedRawDataOffsets_;
 
-        sistrip::FEDReadoutMode mode = sistrip::READOUT_MODE_INVALID;
+      //   fedRawDataOffsets_.clear();
+      //   fedIndex_.clear();
+      //   fedIndex_.resize(sistrip::NUMBER_OF_FEDS, stripgpu::invalidFed);
 
-        for (size_t fedi = 0; fedi < buffers.size(); ++fedi) {
-          auto& buff = buffers[fedi];
-          if (buff != nullptr) {
-            const auto raw = rawdata[fedi];
-            memcpy(fedRawDataHost.get() + off, raw->data(), raw->size());
-            fedIndex_[stripgpu::fedIndex(fedi)] = fedRawDataOffsets_.size();
-            fedRawDataOffsets_.push_back(off);
-            off += raw->size();
-            if (fedRawDataOffsets_.size() == 1) {
-              mode = buff->readoutMode();
-            } else {
-              if (buff->readoutMode() != mode) {
-                throw cms::Exception("[SiStripRawToClusterGPUKernel] inconsistent readout mode ")
-                    << buff->readoutMode() << " != " << mode;
-              }
-            }
-          }
-        }
+      //   sistrip::FEDReadoutMode mode = sistrip::READOUT_MODE_INVALID;
 
-      size_t offset = 0;
+      //   for (size_t fedi = 0; fedi < buffers.size(); ++fedi) {
+      //     auto& buff = buffers[fedi];
+      //     if (buff != nullptr) {
+      //       const auto raw = rawdata[fedi];
+      //       memcpy(fedRawDataHost.get() + off, raw->data(), raw->size());
+      //       fedIndex_[stripgpu::fedIndex(fedi)] = fedRawDataOffsets_.size();
+      //       fedRawDataOffsets_.push_back(off);
+      //       off += raw->size();
+      //       if (fedRawDataOffsets_.size() == 1) {
+      //         mode = buff->readoutMode();
+      //       } else {
+      //         if (buff->readoutMode() != mode) {
+      //           throw cms::Exception("[SiStripRawToClusterGPUKernel] inconsistent readout mode ")
+      //               << buff->readoutMode() << " != " << mode;
+      //         }
+      //       }
+      //     }
+      //   }
+
+      // size_t offset = 0;
 
       // iterate over the detector in DetID/APVPair order
       // mapping out where the data are
-      for (size_t i = 0; i < detView.metadata().size(); ++i) {
-        auto thisDet = detView[i];
-        const auto fedId = thisDet.fedID();
-        const auto fedCh = thisDet.fedCh();
-        const auto fedi = fedIndex_[stripgpu::fedIndex(fedId)];
+      // for (size_t i = 0; i < detView.metadata().size(); ++i) {
+      //   auto thisDet = detView[i];
+      //   const auto fedId = thisDet.fedID();
+      //   const auto fedCh = thisDet.fedCh();
+      //   const auto fedi = fedIndex_[stripgpu::fedIndex(fedId)];
 
-        // if (fedi != invalidFed) {
-        //   const auto buffer = buffers[fedId].get();
-        //   const auto& channel = buffer->channel(thisDet.fedCh());
+      //   // if (fedi != invalidFed) {
+      //   //   const auto buffer = buffers[fedId].get();
+      //   //   const auto& channel = buffer->channel(thisDet.fedCh());
 
-        //   auto len = channel.length();
-        //   auto off = channel.offset();
+      //   //   auto len = channel.length();
+      //   //   auto off = channel.offset();
 
-        //   assert(len >= headerlen || len == 0);
+      //   //   assert(len >= headerlen || len == 0);
 
-        //   if (len >= headerlen) {
-        //     len -= headerlen;
-        //     off += headerlen;
-        //   }
+      //   //   if (len >= headerlen) {
+      //   //     len -= headerlen;
+      //   //     off += headerlen;
+      //   //   }
 
-        //   // to SoA column setting
-        //   chanlocs->setChannelLoc(i, channel.data(), off, offset, len, fedId, fedCh, detp.detID()); 
-        //   inputGPU[i] = fedRawDataGPU.get() + fedRawDataOffsets_[fedi] + (channel.data() - rawdata[fedId]->data());
-        //   offset += len;
+      //   //   // to SoA column setting
+      //   //   chanlocs->setChannelLoc(i, channel.data(), off, offset, len, fedId, fedCh, detp.detID()); 
+      //   //   inputGPU[i] = fedRawDataGPU.get() + fedRawDataOffsets_[fedi] + (channel.data() - rawdata[fedId]->data());
+      //   //   offset += len;
 
-        // } else {
-        //   chanlocs->setChannelLoc(i, nullptr, 0, 0, 0, invalidFed, 0, invalidDet);
-        //   inputGPU[i] = nullptr;
-        // }
-      }
+      //   // } else {
+      //   //   chanlocs->setChannelLoc(i, nullptr, 0, 0, 0, invalidFed, 0, invalidDet);
+      //   //   inputGPU[i] = nullptr;
+      //   // }
+      // }
 
-      const auto n_strips = offset;
+      // const auto n_strips = offset;
 
-      std::cout << "n strips: " << n_strips << std::endl;
+      // std::cout << "n strips: " << n_strips << std::endl;
         
 
       }  // end clusterizer scope
--- RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClustersKernels.h
+++ RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClustersKernels.h
@@ -1,5 +1,5 @@
-#ifndef RecoLocalTracker_SiStripClusterizer_SiStripRawToClusterKernel_h
-#define RecoLocalTracker_SiStripClusterizer_SiStripRawToClusterKernel_h
+#ifndef RecoLocalTracker_SiStripClusterizer_SiStripRawToClustersKernels_h
+#define RecoLocalTracker_SiStripClusterizer_SiStripRawToClustersKernels_h
 
 #include <algorithm>
 #include <optional>
@@ -8,136 +8,35 @@
 #include "HeterogeneousCore/AlpakaInterface/interface/config.h"
 #include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
 
-#include "DataFormats/SiPixelClusterSoA/interface/alpaka/SiPixelClustersSoACollection.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersDevice.h"
-#include "DataFormats/SiPixelDigiSoA/interface/SiPixelDigisDevice.h"
-#include "DataFormats/SiPixelDigiSoA/interface/alpaka/SiPixelDigisSoACollection.h"
-#include "DataFormats/SiPixelDigiSoA/interface/alpaka/SiPixelDigiErrorsSoACollection.h"
-#include "DataFormats/SiPixelDigiSoA/interface/SiPixelDigiErrorsDevice.h"
-#include "DataFormats/SiPixelClusterSoA/interface/ClusteringConstants.h"
-
-#include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibrationForHLTLayout.h"
-#include "CondFormats/SiPixelObjects/interface/alpaka/SiPixelGainCalibrationForHLTDevice.h"
-#include "CondFormats/SiPixelObjects/interface/alpaka/SiPixelMappingDevice.h"
-
-#include "DataFormats/SiStripRawData/interface/SiPixelErrorCompact.h"
-#include "DataFormats/SiStripRawData/interface/SiPixelFormatterErrors.h"
-#include "DataFormats/SiPixelDetId/interface/PixelChannelIdentifier.h"
-
 #include "CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoA.h"
-
-namespace pixelDetails {
-
-  constexpr auto MAX_LINK = pixelgpudetails::MAX_LINK;
-  constexpr auto MAX_SIZE = pixelgpudetails::MAX_SIZE;
-  constexpr auto MAX_ROC = pixelgpudetails::MAX_ROC;
-
-  // Phase 1 geometry constants
-  constexpr uint32_t layerStartBit = 20;
-  constexpr uint32_t ladderStartBit = 12;
-  constexpr uint32_t moduleStartBit = 2;
-
-  constexpr uint32_t panelStartBit = 10;
-  constexpr uint32_t diskStartBit = 18;
-  constexpr uint32_t bladeStartBit = 12;
-
-  constexpr uint32_t layerMask = 0xF;
-  constexpr uint32_t ladderMask = 0xFF;
-  constexpr uint32_t moduleMask = 0x3FF;
-  constexpr uint32_t panelMask = 0x3;
-  constexpr uint32_t diskMask = 0xF;
-  constexpr uint32_t bladeMask = 0x3F;
-
-  constexpr uint32_t LINK_bits = 6;
-  constexpr uint32_t ROC_bits = 5;
-  constexpr uint32_t DCOL_bits = 5;
-  constexpr uint32_t PXID_bits = 8;
-  constexpr uint32_t ADC_bits = 8;
-
-  // special for layer 1
-  constexpr uint32_t LINK_bits_l1 = 6;
-  constexpr uint32_t ROC_bits_l1 = 5;
-  constexpr uint32_t COL_bits_l1 = 6;
-  constexpr uint32_t ROW_bits_l1 = 7;
-  constexpr uint32_t OMIT_ERR_bits = 1;
-
-  constexpr uint32_t maxROCIndex = 8;
-  constexpr uint32_t numRowsInRoc = 80;
-  constexpr uint32_t numColsInRoc = 52;
-
-  constexpr uint32_t MAX_WORD = 2000;
-
-  constexpr uint32_t ADC_shift = 0;
-  constexpr uint32_t PXID_shift = ADC_shift + ADC_bits;
-  constexpr uint32_t DCOL_shift = PXID_shift + PXID_bits;
-  constexpr uint32_t ROC_shift = DCOL_shift + DCOL_bits;
-  constexpr uint32_t LINK_shift = ROC_shift + ROC_bits_l1;
-  // special for layer 1 ROC
-  constexpr uint32_t ROW_shift = ADC_shift + ADC_bits;
-  constexpr uint32_t COL_shift = ROW_shift + ROW_bits_l1;
-  constexpr uint32_t OMIT_ERR_shift = 20;
-
-  constexpr uint32_t LINK_mask = ~(~uint32_t(0) << LINK_bits_l1);
-  constexpr uint32_t ROC_mask = ~(~uint32_t(0) << ROC_bits_l1);
-  constexpr uint32_t COL_mask = ~(~uint32_t(0) << COL_bits_l1);
-  constexpr uint32_t ROW_mask = ~(~uint32_t(0) << ROW_bits_l1);
-  constexpr uint32_t DCOL_mask = ~(~uint32_t(0) << DCOL_bits);
-  constexpr uint32_t PXID_mask = ~(~uint32_t(0) << PXID_bits);
-  constexpr uint32_t ADC_mask = ~(~uint32_t(0) << ADC_bits);
-  constexpr uint32_t ERROR_mask = ~(~uint32_t(0) << ROC_bits_l1);
-  constexpr uint32_t OMIT_ERR_mask = ~(~uint32_t(0) << OMIT_ERR_bits);
-
-  struct DetIdGPU {
-    uint32_t rawId;
-    uint32_t rocInDet;
-    uint32_t moduleId;
-  };
-
-  struct Pixel {
-    uint32_t row;
-    uint32_t col;
-  };
-
-  ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE constexpr pixelchannelidentifierimpl::Packing packing() {
-    return PixelChannelIdentifier::thePacking;
-  }
-
-  ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE constexpr uint32_t pack(uint32_t row,
-                                                              uint32_t col,
-                                                              uint32_t adc,
-                                                              uint32_t flag = 0) {
-    constexpr pixelchannelidentifierimpl::Packing thePacking = packing();
-    adc = std::min(adc, uint32_t(thePacking.max_adc));
-
-    return (row << thePacking.row_shift) | (col << thePacking.column_shift) | (adc << thePacking.adc_shift);
-  }
-
-  constexpr uint32_t pixelToChannel(int row, int col) {
-    constexpr pixelchannelidentifierimpl::Packing thePacking = packing();
-    return (row << thePacking.column_width) | col;
-  }
-
-}  // namespace pixelDetails
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "EventFilter/SiStripRawToDigi/interface/SiStripFEDBuffer.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/OneToManyAssoc.h"
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
-    class SiStripRawToClusterKernel {
+    class SiStripRawToClustersKernels {
     public:
-      SiStripRawToClusterKernel() : nModules_Clusters_h{cms::alpakatools::make_host_buffer<uint32_t[], Platform>(3u)} {}
 
-      ~SiStripRawToClusterKernel() = default;
+      using SequentialContainer = cms::alpakatools::OneToManyAssocSequential<uint32_t, -1, -1>;
+      using SequentialContainerStorage = typename SequentialContainer::index_type;
+      using SequentialContainerView = typename SequentialContainer::View;
+      using SequentialContainerOffsets = typename SequentialContainer::Counter;
+      
+      SiStripRawToClustersKernels(){}
 
-      SiStripRawToClusterKernel(const SiStripRawToClusterKernel&) = delete;
-      SiStripRawToClusterKernel(SiStripRawToClusterKernel&&) = delete;
-      SiStripRawToClusterKernel& operator=(const SiStripRawToClusterKernel&) = delete;
-      SiStripRawToClusterKernel& operator=(SiStripRawToClusterKernel&&) = delete;
+      ~SiStripRawToClustersKernels() = default;
+
+      SiStripRawToClustersKernels(SiStripRawToClustersKernels&&) = delete;
+      SiStripRawToClustersKernels& operator=(const SiStripRawToClustersKernels&) = delete;
+      SiStripRawToClustersKernels& operator=(SiStripRawToClustersKernels&&) = delete;
 
       void makeStripClustersAsync(Queue& queue,
                                    const std::vector<const FEDRawData*>& rawdata,
                                    const std::vector<std::unique_ptr<sistrip::FEDBuffer>>& buffers,
-                                   const StripConditionsDetSoAConstView& detView,
-                                   const StripConditionsStripSoAConstView& stripView,
-                                   const StripConditionsAPVSoAConstView& apvView
+                                   const ::reco::StripConditionsDetSoAConstView& detView,
+                                   const ::reco::StripConditionsStripSoAConstView& stripView,
+                                   const ::reco::StripConditionsAPVSoAConstView& apvView
                                    );
 
     //   SiPixelDigisSoACollection getDigis() {
@@ -155,16 +54,16 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     //   auto nModules() { return nModules_Clusters_h[0]; }
 
     private:
-    //   uint32_t nDigis = 0;
+      uint32_t nStrips_ = 0;
 
       // Data to be put in the event
-      cms::alpakatools::host_buffer<uint32_t[]> nModules_Clusters_h;
-      std::optional<SiPixelDigisSoACollection> digis_d;
-      std::optional<SiPixelClustersSoACollection> clusters_d;
-      std::optional<SiPixelDigiErrorsSoACollection> digiErrors_d;
+      // cms::alpakatools::host_buffer<uint32_t[]> nModules_Clusters_h;
+      // std::optional<SiPixelDigisSoACollection> digis_d;
+      // std::optional<SiPixelClustersSoACollection> clusters_d;
+      // std::optional<SiPixelDigiErrorsSoACollection> digiErrors_d;
     };
 
  
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE
 
-#endif  // plugin_SiPixelClusterizer_alpaka_SiStripRawToClusterKernel_h
+#endif  // plugin_SiPixelClusterizer_alpaka_SiStripRawToClustersKernels_h
