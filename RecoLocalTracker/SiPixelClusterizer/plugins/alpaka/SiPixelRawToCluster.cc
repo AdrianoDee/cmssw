8d5fc8422cf80276be0a1b4c93e353d53831bf2a
Matej Roguljic <matej.roguljic@gmail.com>
2025-03-10 12:10:35 +0100

    Sorting digis with an histogram

--- RecoLocalTracker/SiPixelClusterizer/plugins/alpaka/SiPixelRawToCluster.cc
+++ RecoLocalTracker/SiPixelClusterizer/plugins/alpaka/SiPixelRawToCluster.cc
@@ -72,6 +72,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     PixelDataFormatter::Errors errors_;
 
     const bool includeErrors_;
+    const bool sortDigis_;
     const bool useQuality_;
     uint32_t nDigis_;
     const SiPixelClusterThresholds clusterThresholds_;
@@ -87,6 +88,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
         cablingMapToken_(esConsumes<SiPixelFedCablingMap, SiPixelFedCablingMapRcd>(
             edm::ESInputTag("", iConfig.getParameter<std::string>("CablingMapLabel")))),
         includeErrors_(iConfig.getParameter<bool>("IncludeErrors")),
+        sortDigis_(iConfig.getParameter<bool>("SortDigis")),
         useQuality_(iConfig.getParameter<bool>("UseQualityInfo")),
         clusterThresholds_{iConfig.getParameter<int32_t>("clusterThreshold_layer1"),
                            iConfig.getParameter<int32_t>("clusterThreshold_otherLayers"),
@@ -109,6 +111,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
   void SiPixelRawToCluster<TrackerTraits>::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<bool>("IncludeErrors", true);
+    desc.add<bool>("SortDigis", false);
     desc.add<bool>("UseQualityInfo", false);
     // Note: this parameter is obsolete: it is ignored and will have no effect.
     // It is kept to avoid breaking older configurations, and will not be printed in the generated cfi.py file.
@@ -250,6 +253,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
                                   fedCounter,
                                   useQuality_,
                                   includeErrors_,
+                                  sortDigis_,
                                   edm::MessageDrop::instance()->debugEnabled);
   }
 
