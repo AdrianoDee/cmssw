5863089e6940b8c774aa2e42af93aa659f5e708f
adiflori <adriano.di.florio@cern.ch>
2026-09-13 23:11:26 +0200

    Update trivial serialisation

--- DataFormats/TrackingRecHitSoA/plugins/alpaka/TrivialSerialisation.cc
+++ DataFormats/TrackingRecHitSoA/plugins/alpaka/TrivialSerialisation.cc
@@ -1,3 +1,12 @@
+// #include "DataFormats/TrackingRecHitSoA/interface/TrackingRecHitsHost.h"
+// #include "DataFormats/TrackingRecHitSoA/interface/TrackingRecHitsMaskingHost.h"
+// #include "DataFormats/TrackingRecHitSoA/interface/alpaka/TrackingRecHitsSoACollection.h"
+// #include "DataFormats/TrackingRecHitSoA/interface/alpaka/TrackingRecHitsMaskingSoACollection.h"
+// #include "HeterogeneousCore/TrivialSerialisation/interface/alpaka/SerialiserFactoryDevice.h"
+
+// DEFINE_TRIVIAL_SERIALISER_PORTABLE_PLUGIN(reco::TrackingRecHitHost, reco::TrackingRecHitsSoACollection);
+// DEFINE_TRIVIAL_SERIALISER_PORTABLE_PLUGIN(reco::TrackingRecHitHost, reco::TrackingRecHitsMaskingSoACollection);
+
 #include "DataFormats/TrackingRecHitSoA/interface/alpaka/TrackingRecHitsSoACollection.h"
 #include "DataFormats/TrackingRecHitSoA/interface/alpaka/TrackingRecHitsMaskingSoACollection.h"
 #include "HeterogeneousCore/TrivialSerialisation/interface/alpaka/SerialiserFactoryDevice.h"
