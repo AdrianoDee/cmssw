0dfe0a75d8c1e296187dcb5b438fd0e4ec989373
adiflori <adriano.di.florio@cern.ch>
2026-09-13 23:13:10 +0200

    Local and beamspost offline GPU

--- RecoVertex/BeamSpotProducer/python/BeamSpot_cff.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpot_cff.py
@@ -6,7 +6,9 @@ from RecoVertex.BeamSpotProducer.beamSpotDeviceProducer_cfi import beamSpotDevic
 offlineBeamSpotTask = cms.Task(offlineBeamSpot)
 
 from Configuration.ProcessModifiers.alpaka_cff import alpaka
+from Configuration.ProcessModifiers.trackingGPUOffline_cff import trackingGPUOffline
+
 _offlineBeamSpotTask_alpaka = offlineBeamSpotTask.copy()
 offlineBeamSpotDevice = _beamSpotDeviceProducer.clone(src = cms.InputTag('offlineBeamSpot'))
 _offlineBeamSpotTask_alpaka.add(offlineBeamSpotDevice)
-alpaka.toReplaceWith(offlineBeamSpotTask, _offlineBeamSpotTask_alpaka)
+(alpaka | trackingGPUOffline).toReplaceWith(offlineBeamSpotTask, _offlineBeamSpotTask_alpaka)
